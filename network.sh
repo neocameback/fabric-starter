7a07263e4c12271835926d9b4e349a3fc21cf452
Kochetkov Aleksandr <aleksandr.kochetkov@altoros.com>
2019-04-29 12:15:13 +0300

    Switch nginx from 80 to WWW_PORT

--- network.sh
+++ network.sh
@@ -381,6 +381,7 @@ function generatePeerArtifacts() {
     # nginx proxy config
     sed -e "s/DOMAIN/$DOMAIN/g" \
         -e "s/ORG/$org/g" \
+        -e "s/WWW_PORT/$www_port/g" \
         -e "s/API_PORT/${DEFAULT_API_PORT}/g" \
         $TEMPLATES_ARTIFACTS_FOLDER/nginx.conf > $GENERATED_ARTIFACTS_FOLDER/"nginx-$org.conf"
 
